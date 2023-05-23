3d850a2cfa4b792b8edd07d0f789286a0e6abe5c
Otipst <pitokhirahman@gmail.com>
2023-05-23 21:57:14 +0700

    Adding all files

---
+++ src/my/cafe/MyCafe.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package my.cafe;
+
+/**
+ *
+ * @author nural
+ */
+public class MyCafe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login newlogin = new login();
+        newlogin.setVisible(true);
+    }
+    
+}
